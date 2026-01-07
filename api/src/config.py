7725e0d0504c786740f60d387b342cede5d4cdbe
Bogdan Rizescu <rizescubogdan2001@gmail.com>
2026-01-07 14:48:08 +0100

    Added API deployment changes (#3)
    * Added deployment changes
    
    * Removed unnecessary files and added param to check if connection to db is dead before sending request to it
    
    * testing autodeployment on render
    
    * Use Settings class instead of dotenv
    
    * Added check for DATABASE_URL not null

--- api/src/config.py
+++ api/src/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     JWT_SECRET: str
     JWT_REFRESH_SECRET: str
+    DATABASE_URL: str
 
     class Config:
         env_file = ".env"
--- api/src/database.py
+++ api/src/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgrespw@localhost:5432"
+from config import settings
 
-engine = create_engine(DATABASE_URL)
+if settings.DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set. Please add it to the .env file.")
+
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
